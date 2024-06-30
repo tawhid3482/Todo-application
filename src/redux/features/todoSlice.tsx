bfe5cd0a662c770235c6e871dccabb6ef37340c9
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-30 07:22:39 +0600

    todoSlice

---
+++ src/redux/features/todoSlice.tsx
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = {
+    todos:[],
+};
+
+export const todoSlice = createSlice({
+    name: 'toso',
+    initialState,
+    reducers: {}
+
+})
